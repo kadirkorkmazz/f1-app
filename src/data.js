d87d2e0b694118c39bfc6194810036183f51ab0e
Kadir Korkmaz <kadir33korkmaz@gmail.com>
2022-04-04 14:06:38 +0300

    bug fix

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+import API_KEY from "./constants"
 
 export const getDataFromApi = async () => {
-    return await axios.get("/formula1/trial/v2/en/sport_events/sr:stage:937185/summary.json?api_key=am4s6tqfm68rf9hkt9dd9pvw", {mode:'cors'})
+    return await axios.get(`/formula1/trial/v2/en/sport_events/sr:stage:937185/summary.json?api_key=${API_KEY}`, {mode:'cors'})
     .then(response => response.data)
 }
 
